fd84b7499ee3ac75ca879b19b8e3af14764ae1e1
drd-2rcl <drd.rclrd@gmail.com>
2019-03-31 15:33:47 -0300

    save data in local storage, finish app.

--- app/todo.js
+++ app/todo.js
@@ -2,11 +2,13 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-  'Fazer café',
-  'Estudar Javascript',
-  'Acessar comunidade open source'
-];
+// var todos = [
+//   'Fazer café',
+//   'Estudar Javascript',
+//   'Acessar comunidade open source'
+// ];
+
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [] ;
 
 function renderTodos() {
   listElement.innerHTML = '';
@@ -36,7 +38,7 @@ function addTodo() {
   todos.push(todoText);
   inputElement.value = '';
   renderTodos();
-  console.log(todos)
+  saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
@@ -44,4 +46,9 @@ buttonElement.onclick = addTodo;
 function deleteTodo(pos) {
   todos.splice(pos, 1);
   renderTodos();
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('list_todos', JSON.stringify(todos));
 }
